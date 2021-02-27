9433a9f32550b7d5deef64badfd46f501e0bc39f
Krishna <krishna@Krishnas-MacBook-Pro.local>
2021-02-27 12:33:21 -0800

    slection state added

--- src/App.jsx
+++ src/App.jsx
@@ -12,55 +12,88 @@ class App extends React.Component {
           title: "Lake Inniscarra, Ireland—Pyramid",
           stats: { time: "30:53", distance: "6,248 M" },
           avatarImg: "lake-inniscarra-trainer.png",
+          selected: false,
         },
         {
           coverImg: "performance-series-thumb.jpg",
           title: "Performance Series",
           avatarImg: "performance-series-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "slow-pulls-thumb.jpg",
           title: "Slow Pulls and Fast Intervals",
           stats: { time: "44:13", distance: "9,948 M" },
           avatarImg: "slow-pulls-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "20-minutes-to-toned-thumb.jpg",
           title: "20 Minutes to Toned",
           avatarImg: "20-minutes-to-toned-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "charles-race-thumb.jpg",
           title: "Charles Race, Boston, Massachusetts",
           stats: { time: "36:22", distance: "8,648 M" },
           avatarImg: "charles-race-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "full-body-hiit-thumb.jpg",
           title: "Full-Body HIIT Series",
           avatarImg: "full-body-hiit-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "kafue-river-thumb.jpg",
           title: "Kafue River, Zambia—Power Stroke Pyramid",
           stats: { time: "22:22", distance: "4,660 M" },
           avatarImg: "kafue-river-trainer.jpg",
+          selected: false,
         },
         {
           coverImg: "shred-and-burn-thumb.jpg",
           title: "Shred & Burn Series",
           avatarImg: "shred-and-burn-trainer.jpg",
+          selected: false,
         },
       ],
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event, index) {
+    event.preventDefault();
+
+    let arr = [...this.state.lessons];
+    arr[index].selected = !arr[index].selected;
+    this.setState({ lessons: arr });
   }
 
   render() {
     return (
       <div className="container py-5">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-2 g-md-3">
-          {this.state.lessons.map((lesson) => (
-            <Lesson lesson={lesson} />
+          {this.state.lessons.map((lesson, index) => (
+            <div className="col d-flex align-items-stretch">
+              <div
+                className={
+                  lesson.selected
+                    ? "card lesson-card-selected"
+                    : "card lesson-card"
+                }
+              >
+                <Lesson lesson={lesson} />
+                <a
+                  href="#"
+                  className="stretched-link"
+                  onClick={(event) => this.handleClick(event, index)}
+                ></a>
+              </div>
+            </div>
           ))}
         </div>
       </div>
--- src/Components/Lesson.jsx
+++ src/Components/Lesson.jsx
@@ -3,42 +3,43 @@ import React from "react";
 class Lesson extends React.Component {
   render() {
     return (
-      <div className="col d-flex align-items-stretch">
-        <div className="card shadow-sm">
-          <img
-            className="card-image-top"
-            src={
-              process.env.PUBLIC_URL + "/Images/" + this.props.lesson.coverImg
-            }
-            alt={this.props.lesson.title}
-          />
-          <div className="card-body">
-            <div className="d-flex justify-content-between align-items-top">
-              <p className="card-text">{this.props.lesson.title}</p>
+      <>
+        <img
+          className="card-image-top"
+          src={process.env.PUBLIC_URL + "/Images/" + this.props.lesson.coverImg}
+          alt={this.props.lesson.title}
+        />
+        <div className="card-body">
+          <div className="d-flex justify-content-between align-items-top">
+            <p className="card-text">{this.props.lesson.title}</p>
+            <small className="text-muted">
+              <img
+                src={
+                  process.env.PUBLIC_URL +
+                  "/Images/" +
+                  this.props.lesson.avatarImg
+                }
+                alt={this.props.lesson.title}
+              />
+            </small>
+          </div>
+          {this.props.lesson.stats && (
+            <div className="d-flex">
               <small className="text-muted">
-                <img
-                  src={
-                    process.env.PUBLIC_URL +
-                    "/Images/" +
-                    this.props.lesson.avatarImg
-                  }
-                  alt={this.props.lesson.title}
-                />
+                <i className="bi bi-clock"></i>
+                {this.props.lesson.stats.time} &nbsp;
+                <i className="bi bi-cone"></i>
+                {this.props.lesson.stats.distance}
               </small>
             </div>
-            {this.props.lesson.stats && (
-              <div className="d-flex">
-                <small className="text-muted">
-                  <i className="bi bi-clock"></i>
-                  {this.props.lesson.stats.time} &nbsp;
-                  <i className="bi bi-cone"></i>
-                  {this.props.lesson.stats.distance}
-                </small>
-              </div>
-            )}
-          </div>
+          )}
+          {this.props.lesson.selected && (
+            <button className="px-0 mx-0 btn btn-link lesson-card-button">
+              VIEW DETAILS
+            </button>
+          )}
         </div>
-      </div>
+      </>
     );
   }
 }
